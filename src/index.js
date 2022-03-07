925bd0a5d7886665608d32d61c579f1eb926ed58
Gérard <wooolowizard@gmail.com>
2022-03-07 14:42:46 +0100

    restoring after failed npm export

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Snake from "./package/Snake"
+import Snake from "./package/Snake";
+//import Snake from "react-jungle-snake";
 
 ReactDOM.render(<Snake />, document.getElementById('root'));
